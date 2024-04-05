8d104bc697afaac232a2e7e8d99e3fef51fac438
Chris G <gomezc@whitman.edu>
2024-04-05 14:30:38 -0700

    user sign up

--- Manager.py
+++ Manager.py
@@ -6,20 +6,46 @@ class Manager (DBConnector):
 
     
     """
-    def __init__(self, isUser):
+    def __init__(self):
         super().__init__()
-        isUser = isUser
     
-    def user_sign_up():
-        pass
-    
+    def user_sign_up(self,username, email, fname, lname):
+        self.open_connection()
+        # insert into database.
+        try:
+            
+            query = ("INSERT INTO User" "(Username, Email, FirstName, LastName)" "VALUES(%s, %s, %s, %s)")
+            query_data = (username, email, fname, lname)
+            self.cursor.execute(query, query_data)
+            self.cnx.commit()
+        
+        except Exception as e:
+            print(f"Error!!: {e}")
+
+        finally:
+            self.close_connection()
+        
     def user_log_in():
         pass
     
     def upload_photo():
         pass
     
+    def comment():
+        pass
     
+    def rate():
+        pass
+    
+    def tags():
+        pass
+
+def main():
+    m = Manager()
+    m.user_sign_up("shahad", "shah.gmail.com", "shaha", "dehoti")
+    
+    
+main()
     
     
     
