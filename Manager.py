f8540c1451bfefb60ca7b00128007b7052041c5d
Chris G <gomezc@whitman.edu>
2024-04-05 16:02:21 -0700

    embedded sql work with shaha

--- Manager.py
+++ Manager.py
@@ -29,34 +29,51 @@ class Manager (DBConnector):
         self.open_connection()
         
         # insert into database.
-        try:
-            # open the image.
-            with open(image, "rb") as image_file:
-                image_blob = image_file.read()
-                
-            query = ("INSERT INTO Photo" "(PID, Username, Title, Description, DateUploaded, Image, Location)" "VALUES(%s, %s, %s, %s, %s, %s, %s)")    
-            query_data = (pid, username, title, description, date_uploaded, image_blob, location)
-            self.cursor.execute(query, query_data)
-            self.cnx.commit()
+        query = ("INSERT INTO Photo" "(PID, Username, Title, Description, DateUploaded, Image, Location)" "VALUES(%s, %s, %s, %s, %s, %s, %s)")    
+        query_data = (pid, username, title, description, date_uploaded, image, location)
+        self.cursor.execute(query, query_data)
+        self.cnx.commit()
+    
+    
+        self.close_connection()
+    
+    def comment(self, pid, daytime, username, content):
+        self.open_connection()
         
-        except Exception as e:
-            print(f"Error!!: {e}")
-
-        finally:
-            self.close_connection()
+        query = ("INSERT INTO Comments" "(PID, DayTime, Username, Content)" "VALUES(%s, %s, %s, %s)")
+        query_data = (pid, daytime, username, content)
+        self.cursor.execute(query, query_data)
+        self.cnx.commit()
+        
+        self.close_connection()
     
-    def comment(self):
-        pass
+    def rate(self, avgRating, pid):
+        self.open_connection()
+        
+        query = ("UPDATE Photo SET AvgRating = %s WHERE PID = %s")
+        query_data = (avgRating, pid)
+        self.cursor.execute(query, query_data)
+        self.cnx.commit()
+        
+        self.close_connection()
     
-    def rate(self):
-        pass
-    
-    def tags(self):
-        pass
+    def tags(self, pid, tag):
+        self.open_connection()
+        
+        query = ("INSERT INTO Tags" "(PID, Tag)" "VALUES(%s, %s)")
+        query_data = (pid, tag)
+        self.cursor.execute(query, query_data)
+        self.cnx.commit()
+        
+        self.close_connection()
 
 def main():
     m = Manager()
-    m.user_sign_up("shahad", "shah.gmail.com", "shaha", "dehoti")
-    m.upload_photo(1, "shahad", "pic", "example of uploaded pic",datetime.now().strftime("%Y-%m-%d %H:%M:%S"), './pic_example.jpg', "walla walla")
+    # m.user_sign_up("shahad", "shah.gmail.com", "shaha", "dehoti")
+    # m.upload_photo(1, "shahad", "pic", "example of uploaded pic", datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "neural-network.jpg", "walla walla")
+    # m.comment(1, datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "shahad", "amazing photo!")
+    # m.rate(4.5, 1)
+    # m.tags(1, "#dog")
+    # m.tags(1, "#chris")
     
 main()
