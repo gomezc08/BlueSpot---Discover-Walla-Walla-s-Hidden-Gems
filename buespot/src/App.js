8c4a9fc154d4b2cf41419b8956dae61c8ffc328e
Chris G <gomezc@whitman.edu>
2024-04-18 11:03:53 -0700

    react playing around

--- buespot/src/App.js
+++ buespot/src/App.js
@@ -1,22 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+// main webpage.
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2 className='top-left'> Bluespot</h2>
+        <div class="top-right">
+          <a className="anchor" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Login</a>
+          <a className="anchor" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Sign Up</a>
+        </div>
+
       </header>
     </div>
   );
