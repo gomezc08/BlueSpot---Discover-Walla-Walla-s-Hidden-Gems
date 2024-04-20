ab0a7a11e1ed8f605b91286dd1557f144b72d83f
sdehoti <dehotis@whitman.edu>
2024-04-20 11:37:20 -0700

    installed django

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+// main webpage.
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2 className='top-left'> Chris</h2>
+        <div class="top-right">
+          <a className="anchor" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Login</a>
+          <a className="anchor" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Sign Up</a>
+        </div>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
