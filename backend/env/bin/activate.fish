31d75beeafe5ad948ef4883c8db387aa350968c7
sdehoti <dehotis@whitman.edu>
2024-04-20 12:09:54 -0700

    actually installed django

--- backend/env/bin/activate.fish
+++ backend/env/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/dehotis/Desktop/BlueSpot---Discover-Walla-Walla-s-Hidden-Gems/bluespot-backend/env"
+set -gx VIRTUAL_ENV "/Users/dehotis/Desktop/BlueSpot---Discover-Walla-Walla-s-Hidden-Gems/backend/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
